fc280ce4a82c564788d5f449fdbfe84aa3f736dd
João Vitor <joaovitor.jsr@gmail.com>
2022-09-05 00:44:44 -0300

    update

--- lib/filter_chips_widget.dart
+++ lib/filter_chips_widget.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:state_change/state_change.dart';
 import 'filter_chips_controller.dart';
