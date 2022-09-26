5bbd33b0b02e6d41b36ef522ef4a3d7b419b8caf
João Vitor <joaovitor.jsr@gmail.com>
2022-09-26 19:59:34 -0300

    new

--- lib/filter_chips_widget.dart
+++ lib/filter_chips_widget.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:state_change/state_change.dart';
+
 import 'filter_chips_controller.dart';
+import 'multi_value_listenable_builder.dart';
 
 class FilterChipsWidget extends StatefulWidget {
-  final List<String> filters;
-  final void Function(List<String> data) onChange;
-  const FilterChipsWidget({
-    Key? key,
+  const FilterChipsWidget(
+    this.height, {
+    super.key,
+    this.alignment = Alignment.center,
     required this.filters,
     required this.onChange,
-  }) : super(key: key);
+    this.padding,
+    this.margin,
+  });
+  final void Function(List<String> data) onChange;
+  final List<String> filters;
+  final double height;
+  final AlignmentGeometry alignment;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
 
   @override
   State<FilterChipsWidget> createState() => _FilterChipsWidgetState();
@@ -19,7 +28,7 @@ class _FilterChipsWidgetState extends State<FilterChipsWidget>
     with AutomaticKeepAliveClientMixin {
   late final List<String> _filters;
 
-  final controller = FilterChipsController();
+  final FilterChipsController controller = FilterChipsController();
 
   @override
   void initState() {
@@ -29,55 +38,70 @@ class _FilterChipsWidgetState extends State<FilterChipsWidget>
   }
 
   @override
+  void dispose() {
+    controller.dispose();
+    _filters.clear();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     super.build(context);
     return Container(
-      height: 62,
-      alignment: Alignment.bottomCenter,
-      child: StateChange<List<String>>(
-        notifier: controller,
-        builder: (context, value) => ListView.builder(
-          scrollDirection: Axis.horizontal,
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          shrinkWrap: true,
-          physics: const BouncingScrollPhysics(),
-          primary: true,
-          itemCount: _filters.length,
-          itemBuilder: (context, index) => Padding(
-            padding: const EdgeInsets.only(right: 10.0),
-            child: InkWell(
-              onLongPress: null,
-              child: FilterChip(
-                key: ObjectKey(_filters[index]),
-                shape: RoundedRectangleBorder(
-                  side: const BorderSide(
-                    color: Colors.transparent,
-                    width: 0,
-                    strokeAlign: StrokeAlign.outside,
-                  ),
-                  borderRadius: BorderRadius.circular(100),
-                ),
-                side: const BorderSide(width: 0),
-                shadowColor: Colors.transparent,
-                elevation: 0,
-                checkmarkColor: const Color(0xff2A2939),
-                backgroundColor: const Color(0xff2A2939),
-                selectedColor: const Color.fromARGB(255, 105, 175, 255),
-                label: Text(
-                  _filters[index],
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                selected: value.contains(_filters[index]),
-                onSelected: (value) {
-                  controller.add(_filters[index]);
-                  widget.onChange(controller.value);
+      padding: widget.padding,
+      margin: widget.margin,
+      height: widget.height,
+      alignment: widget.alignment,
+      child: MultiValueListenableBuilder(
+        valueListenables: [
+          controller,
+        ],
+        builder: (context, values, child) {
+          final List<String> lista = values.elementAt(0) as List<String>;
+          return Wrap(
+            spacing: 2,
+            children: [
+              ...List.generate(
+                _filters.length,
+                (index) {
+                  final String filter = _filters[index];
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                      left: 6,
+                      right: 6,
+                    ),
+                    child: InkWell(
+                      onLongPress: null,
+                      child: FilterChip(
+                        key: ObjectKey(filter),
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(
+                            color: Colors.transparent,
+                            width: 0,
+                            strokeAlign: StrokeAlign.outside,
+                          ),
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        side: const BorderSide(width: 0),
+                        label: Text(
+                          filter,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        selected: lista.contains(filter),
+                        onSelected: (value) {
+                          controller.add(filter);
+                          widget.onChange(controller.value);
+                        },
+                      ),
+                    ),
+                  );
                 },
-              ),
-            ),
-          ),
-        ),
+              )
+            ],
+          );
+        },
       ),
     );
   }
